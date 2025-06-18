8b4affed8cd30165504eea3d5affad78f4b50d23
VISHAL DUBEY <DUBEYVISHAL917@GMAIL.COM>
2025-06-18 19:19:32 +0530

    Add files via upload

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Toaster } from "sonner";
 import "./globals.css";
-
+import { SpeedInsights } from "@vercel/speed-insights/next";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -49,6 +49,7 @@ export default function RootLayout({
       >
         <Toaster position="top-center" richColors />
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
